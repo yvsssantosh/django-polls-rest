31542418affb83c3bd6eb78b69c8e1bb1a462be1
Yadavalli Santosh <yvss.santosh@gmail.com>
2019-01-21 22:01:30 +0530

    Added tests using Hypothesis

--- polls/tests.py
+++ polls/tests.py
@@ -1,50 +1,72 @@
-from django.contrib.auth import get_user_model
-from rest_framework.test import APITestCase
-from rest_framework.test import APIRequestFactory
-from rest_framework.test import APIClient
+# Default imports
+import time
+from hypothesis import given, settings, strategies as st
+from django.contrib.auth.models import User
+from hypothesis.extra.django import TestCase, from_model
+from rest_framework.test import APIClient, APIRequestFactory
 
+# Custom imports
 from polls import apiviews
 
+# Our testing class
+class TestPoll(TestCase):
 
-class TestPoll(APITestCase):
+    # Initial setup
     def setUp(self):
         self.client = APIClient()
         self.factory = APIRequestFactory()
-        self.view = apiviews.PollViewSet.as_view({'get': 'list'})
-        self.uri = '/polls/'
-        self.user = self.setup_user()
+        self.view = apiviews.PollViewSet.as_view({"get": "list"})
+        self.uri = "/polls/"
 
-    @staticmethod
-    def setup_user():
-        User = get_user_model()
-        return User.objects.create_user(
-            'test',
-            email='testuser@test.com',
-            password='test'
+    # Testing create polls
+    @settings(deadline=2000, max_examples=10)
+    @given(from_model(User))
+    def test_create(self, user):
+        user = self.set_password_to_user(user)
+        self.client.login(username=user.username, password="hello")
+        body = {"question": st.text(), "created_by": user.id}
+        time.sleep(1)
+        response = self.client.post(self.uri, body)
+        self.assertEqual(
+            response.status_code,
+            201,
+            "Expected Response Code 201, received {0} instead.".format(
+                response.status_code
+            ),
         )
 
-    def test_list(self):
+    # Testing list polls - Method I
+    @settings(deadline=2000, max_examples=10)
+    @given(from_model(User))
+    def test_list(self, user):
         request = self.factory.get(self.uri)
-        request.user = self.user
+        request.user = user
         response = self.view(request)
-        self.assertEqual(response.status_code, 200,
-                         'Expected Response Code 200, received {0} instead.'
-                         .format(response.status_code))
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Expected Response Code 200, received {0} instead.".format(
+                response.status_code
+            ),
+        )
 
-    def test_list2(self):
-        self.client.login(username='test', password='test')
+    # Testing list polls - Method II
+    @settings(deadline=2000, max_examples=10)
+    @given(from_model(User))
+    def test_list2(self, user):
+        user = self.set_password_to_user(user)
+        self.client.login(username=user.username, password="test")
         response = self.client.get(self.uri)
-        self.assertEqual(response.status_code, 200,
-                         'Expected Response Code 200, received {0} instead.'
-                         .format(response.status_code))
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Expected Response Code 200, received {0} instead.".format(
+                response.status_code
+            ),
+        )
 
-    def test_create(self):
-        self.client.login(username="test", password="test")
-        params = {
-            "question": "How are you?",
-            "created_by": 1
-        }
-        response = self.client.post(self.uri, params)
-        self.assertEqual(response.status_code, 201,
-                         'Expected Response Code 201, received {0} instead.'
-                         .format(response.status_code))
+    # Just a method to set password
+    def set_password_to_user(self, user):
+        user.set_password("hello")
+        user.save()
+        return user
